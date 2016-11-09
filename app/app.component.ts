35f0280f0ced89637f2bfc55fafea2685ff84215
AfreenF <fathima821@gmail.com>
2016-11-09 14:24:13 +0530

    added files related to project

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';			// import the members that we need in our application.
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App. Hello World</h1>'
+})   // this is the component metadata defined by decorator, and template.
+export class AppComponent { } // this is the class, it defines the properties and methods needed by our view.
